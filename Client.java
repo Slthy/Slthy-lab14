b037bf19a8c4c7ea678a49007d9a25f41cbb6431
Alessandro Borsato <75090321+Slthy@users.noreply.github.com>
2025-11-11 17:22:12 -0500

    building client and server

--- Client.java
+++ Client.java
@@ -2,10 +2,67 @@ import java.util.*;
 import java.net.*;
 import java.io.*;
 
+
+/*
+Initial Handshake
+    When a client first connects, they need to send a key to the server to 
+    indicate they’re a valid client (recall that anyone can connect to public ports). 
+    Here, the key will be the very insecure passcode 12345; 
+    the server should look for each client to send such a passcode as their first message, 
+    make sure it matches this number, and only then allow the client to actually 
+    remain connected for future factorization requests.
+
+Client
+    You will implement a client in Client.java that connects to the server with a handshake, 
+    and then sends and receives messages. See the unit tests for details of what methods you are 
+    required to have (and their functionality).
+*/
 public class Client{
+    // client.getSocket().getLocalAddress().toString()
+    // client.getSocket().getPort())
+
+    // client.handshake();
+    private Socket s;
+    private String host;
+    private int port;
+
+    private static String PASSWORD = "12345";
+
+    public Client(String host, int port){
+        this.host = host;
+        this.port = port;
+        this.s = new Socket(this.host, this.port);
+    }
+
+    public void handshake(){
+        try{
+            PrintWriter pw = new PrintWriter(sock.getOutputStream());
+            pw.println(PASSWORD);
+            pw.flush();
+
+            BufferedReader in = new BufferedReader(new InputStreamReader(sock.getInputStream()));
+
+            String reply = in.readLine(); 
+            System.out.println("Server said: " + reply);
+
+            
+            pw.close();
+            in.close();
+            sock.close();
+        }catch(Exception e){
+            System.err.print("IOException");
+            System.exit(1);
+        }
+    }
+
+
 
     public static void main(String args[]){
+        // connect with handshake
 
+        // send and receive message
+
+        // client.request(number.toString());
 
     }
 }
--- Server.java
+++ Server.java
@@ -2,10 +2,107 @@ import java.util.*;
 import java.net.*;
 import java.io.*;
 
+/*
+Initial Handshake
+    When a client first connects, they need to send a key to the server to 
+    indicate they’re a valid client (recall that anyone can connect to public ports). 
+    Here, the key will be the very insecure passcode 12345; 
+    the server should look for each client to send such a passcode as their first message, 
+    make sure it matches this number, and only then allow the client to actually 
+    remain connected for future factorization requests.
+
+Server
+    You will implement a server in Server.java that allows multiple clients to 
+    simultaneously connect to the server, perform a handshake, 
+    and request that the server factorize an integer.
+
+    the serve method
+        To facilitate grading, the server will have a serve method that takes as argument the 
+        number of clients it is expected to serve per test case, as opposed to an infinite 
+        loop like there was in the exercises from class. This serve method will have a loop 
+        run for as many clients as specified in its argument.
+
+        After accepting a client via a successful handshake, this method will process 
+        the client request in a separate thread so that the server can continue to accept 
+        connections while these expensive factorization calculations are being performed on 
+        behalf of various clients.
+        --> serve +1
+
+    other server methods
+        The server should record the time each client was connected so that it can properly return 
+        these values with the getConnectedTimes method, which should return a sorted ArrayList of 
+        LocalDateTime objects representing the connection time of every client.
+
+    See the unit tests for details of what additional methods you are required to have (and their functionality).
+*/
+
 public class Server{
 
-    public static void main(String args[]){
+    ServerSocket serverSock;
 
+    public EchoSocketServer(int port){
+        try{
+            serverSock = new ServerSocket(port);
+        }catch(Exception e){
+            System.err.println("Cannot establish server socket");
+            System.exit(1);
+        }
+        
+    }
+
+    public void serve(){
+        while(true){
+            try{
+                //accept incoming connection
+                Socket clientSock = serverSock.accept();
+                System.out.println("New connection: "+clientSock.getRemoteSocketAddress());
+                //start the thread
+                (new ClientHandler(clientSock)).start();
+                
+                //continue looping
+            }catch(Exception e){} //exit serve if exception
+        }
+    }
+
+    private class ClientHandler extends Thread{
+
+        Socket sock;
+        public ClientHandler(Socket sock){
+            this.sock=sock;
+        }
+
+        public void run(){
+            PrintWriter out=null;
+            BufferedReader in=null;
+            try{
+                out = new PrintWriter(sock.getOutputStream());
+                in = new BufferedReader(new InputStreamReader(sock.getInputStream()));
+
+                //read and echo back forever!
+                while(true){
+                    String msg = in.readLine();
+                    if(msg == null) break; //read null, remote closed
+                    out.println(msg);
+                    out.flush();
+                }
+
+                //close the connections
+                out.close();
+                in.close();
+                sock.close();
+                
+            }catch(Exception e){}
+
+            //note the loss of the connection
+            System.out.println("Connection lost: "+sock.getRemoteSocketAddress());
+        }
 
     }
+    /*
+    public static void main(String args[]){
+        EchoSocketServer server = new EchoSocketServer(2021);
+        server.serve();
+    }
+    */  
+    
 }
